cee13d366b63b29426f873e06a93666160858559
Eric John Adamos <ericjohnadamos@gmail.com>
2024-10-20 21:43:37 +0800

    Add score text (WIP)

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,10 @@ static bool EventTriggered(double interval) {
 
 int main()
 {
-	const int screenWidth = 900;
+	const int screenWidth = 1500;
 	const int screenHeight = 1800;
+
+	Font font = LoadFontEx("Super Kloud.ttf", 64, 0, 0);
 
 	InitWindow(screenWidth, screenHeight, "Tetris Game");
 	SetTargetFPS(60);
@@ -30,6 +32,7 @@ int main()
 			game.MoveBlockDown();
 		BeginDrawing();
 		ClearBackground(BLACK);
+		DrawTextEx(font, "Score", { 365, 50 }, 380, 2, GREEN);
 		game.Draw();
 		EndDrawing();
 	}
